739fb42b2e0c3499848242c7251cae26ce537341
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-20 10:15:03 +0800

    services: increase allowed max cached processes for 6gb+ devices
    Change-Id: Ife66438c6d4a735703cff777be48cdaebd19a96a
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/am/ActivityManagerConstants.java
+++ services/core/java/com/android/server/am/ActivityManagerConstants.java
@@ -2078,10 +2078,15 @@ final class ActivityManagerConstants extends ContentObserver {
         com.android.internal.util.MemInfoReader memInfoReader = new com.android.internal.util.MemInfoReader();
         memInfoReader.readMemInfo();
         final long ramBytes = memInfoReader.getTotalSize();
-        if (ramBytes <= android.util.DataUnit.GIGABYTES.toBytes(4)) {
+
+        final long GB = android.util.DataUnit.GIGABYTES.toBytes(1);
+
+        if (ramBytes <= 4 * GB) {
             CUR_MAX_CACHED_PROCESSES = 32;
+        } else if (ramBytes <= 6 * GB) {
+            CUR_MAX_CACHED_PROCESSES = 48;
         } else {
-            CUR_MAX_CACHED_PROCESSES = 64;
+            CUR_MAX_CACHED_PROCESSES = 128;
         }
         CUR_MAX_CACHED_PROCESSES = Integer.min(CUR_MAX_CACHED_PROCESSES, MAX_CACHED_PROCESSES);
 
